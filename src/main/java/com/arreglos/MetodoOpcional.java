73f100ffa369a527f51bdb36228b1d3db83e2204
luistalero <luisalbertotaleromartrinez@gmail.com>
2025-02-17 16:04:40 -0500

    feat: :sparkles: Commit 2: Termina la clase de metodos

--- src/main/java/com/arreglos/MetodoOpcional.java
+++ src/main/java/com/arreglos/MetodoOpcional.java
@@ -1,5 +1,6 @@
 package com.arreglos;
 
+//import java.util.Scanner;
 import java.util.Optional;
 
 public class MetodoOpcional {
@@ -7,7 +8,22 @@ public class MetodoOpcional {
         System.out.println("Hola, " + nombre.orElse("Invitado") + "!");
     }
     public static void main(String[] args) {
-        saludar(Optional.of("Spock"));
-        saludar(Optional.empty());
+
+        saludar(Optional.of("Luis"));
+        saludar(Optional.empty()); 
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Ingrese su nombre de Registro: ");
+        // String nombre = sc.nextLine();
+
+        // if (nombre == null) {
+        //     saludar(Optional.of(nombre));
+        // } else {
+        //    saludar(Optional.empty()); 
+        // }
+        
+        
+    
+        // sc.close();
     }
 }
